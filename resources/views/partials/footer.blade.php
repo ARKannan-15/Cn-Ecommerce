c315712f78ca03c42cff474a0e1b023fd837658a
kannanAR <77109649+ARKannan-15@users.noreply.github.com>
2022-12-13 22:40:28 +0530

    footer changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,19 +10,19 @@
                     <h3>
                         Welcome to the site
                     </h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                    <p>Do check our website for exciting offers.</p>
                 </div>
                 <div class="col-md-4">
                     <h3>
                         Contact:
                     </h3>
-                    <p>Phone: +249126545060</p>
-                    <p>Email: mohammedomer789@gmail.com</p>
-                    <p>Facebook: <a href="https://facebook.com/mohammedomer789">Mohammed Omer</a></p>
-                    <p>Github: <a href="https://github.com/m7md3omer">Mohammed Omer</a></p>
+                    <p>Phone: +91 7993973862</p>
+                    <p>Email: kannanar464@gmail.com</p>
+                    <p>Website: <a href="https://mhmdomer.com">Personal Website</a></p>
+                    <p>Github: <a href="https://github.com/mhmdomer">@ARkannan-15</a></p>
                 </div>
             </div>
         </div>
-        <p>copyright &copy; Mohammed Omer Ali - All rights reserved 2019</p>
+       <p>copyright &copy; Kannan A.R - All rights reserved 2022</p>
     </div>
 </footer>
